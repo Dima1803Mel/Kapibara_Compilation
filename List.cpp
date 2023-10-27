1d7a1bd90c7a3bd206ec6a649218d0b721e5da90
Mel <dima.melov.03@mail.ru>
2023-10-27 07:43:55 +0300

    first commit

---
+++ List.cpp
@@ -0,0 +1,110 @@
+class Node {
+public:
+	double data;
+	Node* next;
+
+	Node(double data) {
+		this->data = data;
+		this->next = next;
+	}
+};
+
+class List {
+public:
+	Node* head, * tail;
+
+	List() {
+		this->head = this->tail = NULL;
+	}
+
+	~List() {
+		while (head != NULL) pop_front();
+	}
+
+	// удаляет первый элемент списка
+	void pop_front() {
+		if (head == NULL) return;
+		if (head == tail) {
+			delete tail;
+			head = tail = NULL;
+			return;
+		}
+
+		Node* node = head;
+		head = node->next;
+		delete node;
+	}
+
+	// добавляет последний элемент списка
+	void push_back(double data) {
+		Node* node = new Node(data);
+		if (head == NULL) head = node;
+		if (tail != NULL) tail->next = node;
+		tail = node;
+	}
+
+	// добавляет первый элемент списка
+	void push_front(double data) {
+		Node* node = new Node(data);
+		node->next = head;
+		head = node;
+		if (tail == NULL) tail = node;
+	}
+
+	// удаляет последний элемент списка
+	void pop_back() {
+		if (tail == NULL) return;
+		if (head == tail) {
+			delete tail;
+			head = tail = NULL;
+			return;
+		}
+
+		Node* node = head;
+		for (; node->next != tail; node = node->next);
+		node->next = NULL;
+		delete tail;
+		tail = node;
+	}
+
+	// Возвращает элемент списка
+	Node* getAt(int k) {
+		if (k < 0) return NULL;
+		Node* node = head;
+		int n = 0;
+		while (node && n != k && node->next) {
+			node = node->next;
+			n++;
+		}
+		return (n == k) ? node : NULL;
+	}
+
+	// Вставляет элемент списка
+	void insert(int k, double data) {
+		NOde* left = getAt(k);
+		if (left == NULL) return;
+		Node* right = left->next;
+		Node* node = new Node(data);
+
+		left->next = node;
+		node->next = right;
+		if (right == NULL) tail = node;
+	}
+
+	// удаляет элемент списка
+	void erase(int k) {
+		if (k < 0) return;
+		if (k == 0) {
+			pop_front();
+			return;
+		}
+
+		Node* left = getAt(k - 1);
+		Node* node = left->next;
+		if (node == NULL) return;
+		Node* right = node->next;
+		left->next = right;
+		if (node == tail) tail = left;
+		delete node;
+	}
+};
